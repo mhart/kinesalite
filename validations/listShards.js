9a1c9193dff8be6532c7647026da26f89ae240ce
Kailash HD <kdayanand@lyft.com>
2018-03-20 15:05:56 -0700

    Add support for list shards

---
+++ validations/listShards.js
@@ -0,0 +1,20 @@
+exports.types = {
+   Limit: {
+     type: 'Integer',
+     greaterThanOrEqual: 1,
+     lessThanOrEqual: 10000,
+   },
+   ExclusiveStartShardId: {
+     type: 'String',
+     regex: '[a-zA-Z0-9_.-]+',
+     lengthGreaterThanOrEqual: 1,
+     lengthLessThanOrEqual: 128,
+   },
+   StreamName: {
+     type: 'String',
+     notNull: true,
+     regex: '[a-zA-Z0-9_.-]+',
+     lengthGreaterThanOrEqual: 1,
+     lengthLessThanOrEqual: 128,
+   },
+ }
